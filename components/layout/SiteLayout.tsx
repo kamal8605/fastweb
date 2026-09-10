8c9eb49495f7fd76d1528eab8d0afcf09120fa90
kamal8605 <rathodramsing80@gmail.com>
2026-09-10 19:01:53 +0530

    refactor(layout): remove AgeVerification component from SiteLayout

--- components/layout/SiteLayout.tsx
+++ components/layout/SiteLayout.tsx
@@ -2,12 +2,10 @@ import { type ReactNode } from "react";
 import { UtilityBar } from "./UtilityBar";
 import { NavBar } from "./NavBar";
 import { Footer } from "./Footer";
-import { AgeVerification } from "@/components/shared/AgeVerification";
 
 export function SiteLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex min-h-screen w-full min-w-0 flex-col overflow-x-clip">
-      <AgeVerification />
       <UtilityBar />
       <NavBar />
       <main className="min-w-0 flex-1 overflow-x-clip">{children}</main>
